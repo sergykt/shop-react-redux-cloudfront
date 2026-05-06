732e22212e6249abf59b81ad2cd76b74afa113f2
Sergei Sivtsev <sergykt@gmail.com>
2026-05-06 19:34:45 +0600

    chore: update API paths and deployment URLs in README and constants

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://vd7z1vg9zc.execute-api.us-east-1.amazonaws.com/prod",
-  bff: "https://4xxckkyhoj.execute-api.us-east-1.amazonaws.com/prod",
+  import: "https://szmxm0u6hd.execute-api.us-east-1.amazonaws.com/prod",
+  bff: "https://htfzqpzute.execute-api.us-east-1.amazonaws.com/prod",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
